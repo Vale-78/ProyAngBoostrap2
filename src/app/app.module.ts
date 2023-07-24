99ce949fb7ead6d3369aa9e002a486079ab7eccc
Valeria Foglino <valeriafoglino.78@gmail.com>
2023-07-24 12:40:26 -0300

    Inicio Proyecto-primer commit- ya añadí componentes para cada sección

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { IntroComponent } from './intro/intro.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { WhyUsComponent } from './why-us/why-us.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { TeamComponent } from './team/team.component';
+import { ClientsComponent } from './clients/clients.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    IntroComponent,
+    AboutComponent,
+    ServicesComponent,
+    WhyUsComponent,
+    PortfolioComponent,
+    TestimonialsComponent,
+    TeamComponent,
+    ClientsComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
