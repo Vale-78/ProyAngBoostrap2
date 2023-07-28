4376c4f3a1c431a52206da9d34f4311fd8dcfe94
Valeria Foglino <valeriafoglino.78@gmail.com>
2023-07-28 14:08:47 -0300

    Funcione el grid (col6-md) añadiendo boostrap-grid.min.css en angular.json

--- src/app/services/box/box.component.ts
+++ src/app/services/box/box.component.ts
@@ -8,7 +8,7 @@ import { Component, Input } from '@angular/core';
 export class BoxComponent {
   @Input() delay:string='';
   @Input() titulo:string='';
-  @Input() text:string='';
+  @Input() texto:string='';
   @Input() icono:string='';
   @Input() color:string='';
 
